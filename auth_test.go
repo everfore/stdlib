4d8d8b7a2beade8afaef0e9a50be9fa1f55882a4
shaalx <wisdomswing@gmail.com>
2015-05-19 23:06:09 +0800

    oath

--- auth_test.go
+++ auth_test.go
@@ -9,7 +9,7 @@ import (
 func TestAuth(t *testing.T) {
 	vals := url.Values{
 		"client_id":    {"8ba2991113e68b4805c1"},
-		"redirect_uri": {"http://shaalx-echouj.daoapp.io/callback"},
+		"redirect_uri": {"http://echoath.daoapp.io/callback"},
 		"scope":        {"repo"},
 		"state":        {"state"},
 	}
