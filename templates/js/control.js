08d35b2ef27e683e73287a92b203153be92ab0d3
shaalx <1316011195@qq.com>
2015-05-29 20:35:19 +0800

    cpush

--- templates/js/control.js
+++ templates/js/control.js
@@ -1,4 +1,25 @@
-angular.module("app", []).controller("HelloCtrl",
-function ($scope) {
-    $scope.cname="cWorld";
+
+angular.module("FF", []).controller("HelloCtrl",
+	function($scope){
+	$scope.cname = "ANGULARJS_F";
+	$scope.gName = function () {
+		return $scope.cname+"_gName";
+	}
+
+	$scope.kids = [
+		{name:"Nacy",age:25	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+		{name:"Oari",age:27	},
+	];
 });
+angular.module("app", []).controller("HelloCtrl",
+	function ($scope) {
+    $scope.cname="ANGULARJS_A";
+});
+
